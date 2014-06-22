543808e885cf7c4cc1850af9f7d4cb72a7aa1e48
mwvdev <mwvdev@users.noreply.github.com>
2014-06-22 22:42:33 +0200

    Added additional DemoLoader testing.

--- src/test/groovy/DemoLoaderTest.groovy
+++ src/test/groovy/DemoLoaderTest.groovy
@@ -6,6 +6,7 @@ import mwvdev.quake.exceptions.LoaderException
 import mwvdev.quake.loaders.DemoLoader
 import mwvdev.quake.models.DeltaEntityState
 import mwvdev.quake.models.Demo
+import mwvdev.quake.models.EntityState
 import mwvdev.quake.models.Message
 import spock.lang.Specification
 
@@ -23,6 +24,7 @@ class DemoLoaderTest extends Specification {
         Demo demo = demoLoader.loadDemo( channel )
         Message message1 = demo.messages.get( 0 )
         Message message2 = demo.messages.get( 1 )
+        Message message3 = demo.messages.get( 573 )
 
         expect:
         demo.messages.size == 19281
@@ -53,11 +55,11 @@ class DemoLoaderTest extends Specification {
         message2.snapshot.serverTime == 19800
         message2.snapshot.flags == 5
         message2.snapshot.playerState.commandTime == 19740
-        message2.snapshot.playerState.origin[0] == 1272
-        message2.snapshot.playerState.origin[1] == 488
-        message2.snapshot.playerState.viewAngles[1].toString() == "-179.93958"
-        message2.snapshot.playerState.viewAngles[0].toString() == "-0.010986328"
-        message2.snapshot.playerState.origin[2].toString() == "88.19"
+        message2.snapshot.playerState.origin[0] == 1272f
+        message2.snapshot.playerState.origin[1] == 488f
+        message2.snapshot.playerState.viewAngles[1] == -179.93958f
+        message2.snapshot.playerState.viewAngles[0] == -0.010986328f
+        message2.snapshot.playerState.origin[2] == 88.19f
         message2.snapshot.playerState.eventSequence == 1
         message2.snapshot.playerState.torsoAnim == 11
         message2.snapshot.playerState.events[0] == 82
@@ -75,6 +77,44 @@ class DemoLoaderTest extends Specification {
         message2.snapshot.playerState.stats == [115, null, null, 510, null, null, 65, 100, null, null, null, null, null, null, null, null]
         message2.snapshot.playerState.persistent == [null, null, 16384, null, 1, null, null, null, null, null, null, null, null, null, null, null]
         message2.snapshot.playerState.ammo == [null, 65535, 150, 25, 25, 25, 150, 25, 150, null, 65535, null, null, null, null, null]
+
+        message3.sequence == 81716
+        message3.acknowledge == 63
+        message3.snapshot.serverTime == 34875
+        message3.snapshot.flags == 0
+        message3.snapshot.areaMask == [-2]
+        message3.snapshot.playerState.commandTime == 34810
+        message3.snapshot.playerState.origin[0] == 336.49506f
+        message3.snapshot.playerState.origin[1] == -207.10805f
+        message3.snapshot.playerState.bobCycle == 2
+        message3.snapshot.playerState.velocity[0] == 258.06137f
+        message3.snapshot.playerState.velocity[1] == 72.98913f
+        message3.snapshot.playerState.weaponTime == 120
+        message3.snapshot.playerState.origin[2] == 0.32596123f
+        message3.snapshot.playerState.velocity[2] == 0.70607257f
+        message3.snapshot.playerState.legsTimer == 126
+        message3.snapshot.playerState.pmTime == 246
+        message3.snapshot.playerState.eventSequence == 15
+        message3.snapshot.playerState.events[0] == 7
+        message3.snapshot.playerState.legsAnim == -109
+        message3.snapshot.playerState.pmFlags == 4128
+        message3.snapshot.playerState.groundEntityNum == 1022
+        message3.snapshot.playerState.damageYaw == -1
+        message3.snapshot.playerState.damagePitch == -1
+
+        message3.snapshot.playerState.stats == [88, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null]
+        message3.snapshot.playerState.persistent == [null, null, null, null, null, null, 1022, null, null, null, null, null, null, null, null, null]
+
+        message3.snapshot.entityStates.get( 0 ) instanceof EntityState
+        DeltaEntityState entityState = message3.snapshot.entityStates.get( 0 )
+        entityState.number == 6
+        entityState.position.base[0] == 955
+        entityState.position.base[1] == 523
+        entityState.position.delta[0] == 269
+        entityState.position.delta[1] == -382
+        entityState.position.base[2] == 123
+        entityState.angle.base[1] == -48
+        entityState.position.delta[2] == -138
     }
 
     def "should throw loading exception on truncated demo"() {
